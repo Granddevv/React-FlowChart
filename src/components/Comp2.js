01ef273d6517cab711f17b313c76902fd1d115b2
granddevv <spacingflight@outlook.com>
2018-05-04 16:55:36 +0200

    Add Component type 1,2,3 functionality and styling

---
+++ src/components/Comp2.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {addLeft, addRight} from '../store/actions';
+import './Comp2.css';
+
+export class Comp2 extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChangeLeft = this.handleChangeLeft.bind(this);
+        this.handleChangeRight = this.handleChangeRight.bind(this);
+    }
+
+    handleChangeLeft() {
+        const { addLeft } = this.props;
+        addLeft();
+    }
+
+    handleChangeRight() {
+        const { addRight } = this.props;
+        addRight();
+    }
+
+    render() {
+        return(
+            <div className="comp_two_container">
+                <h1>Here is Component 2</h1>
+                <input type="checkbox" onChange={this.handleChangeLeft} />
+                <input type="checkbox" onChange={this.handleChangeRight} />
+            </div>
+    )
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+            addLeft: () => {
+            dispatch(addLeft());
+},
+    addRight: () => {
+        dispatch(addRight());
+    }
+}
+}
+
+export default connect(null, mapDispatchToProps)(Comp2);
