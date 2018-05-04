dd3822870c4490667ae1e888f22801d5f70fdb7c
granddevv <spacingflight@outlook.com>
2018-05-04 16:51:50 +0200

    Add Action for flowchart

---
+++ src/store/actions/index.js
@@ -0,0 +1,15 @@
+export const ADD_LEFT = 'ADD_LEFT';
+export const ADD_RIGHT = 'ADD_RIGHT';
+
+export function addLeft() {
+    return {
+        type: ADD_LEFT
+    }
+}
+
+export function addRight() {
+    return {
+        type: ADD_RIGHT
+    }
+}
+
